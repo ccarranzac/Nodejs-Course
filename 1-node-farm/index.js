e194f4c8d232f2e57bf21c357458d78c1a003d83
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-05-18 11:01:13 -0500

    read and write file async

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,25 @@
 const fs = require('fs')
+// Blocking synchronous way
+// const textIn=fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn);
 
-const textIn=fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(textIn);
+// const textOut= `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('File written');
 
-const textOut= `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`
-fs.writeFileSync('./txt/output.txt', textOut)
-console.log('File written');
+//Non-Blocking asynchronous way
+fs.readFile('./txt/start.txt','utf-8', (error, data1)=>{
+    if(error) return console.log("Error: "+error);
+    fs.readFile(`./txt/${data1}.txt`,'utf-8', (error, data2)=>{
+        console.log(data2);
+        fs.readFile('./txt/append.txt','utf-8', (error, data3)=>{
+            console.log(data3);
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8', error=>{
+                console.log("file has been written");
+            })
+        })
+    })
+})
+console.log("reading file");
+
+
