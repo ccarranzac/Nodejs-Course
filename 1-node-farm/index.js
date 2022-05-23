ff62f9df5b2f635e575afa69dbe5fe360343c52b
Camilo Andrés Carranza <ccarranzac@slabcode.com>
2022-05-23 15:12:14 -0500

    npm intro

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const http = require('http')
 const url=require('url')
+const slugify=require('slugify')
 const replaceTemplate=require('./modules/replaceTemplate')
 
 ////////////////////// Files ///////////////////////////////
@@ -36,7 +37,9 @@ const tempProduct=fs.readFileSync(`${__dirname}/templates/template-product.html`
 const data=fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const productData= JSON.parse(data)
 
-
+const slugs= productData.map(x => slugify(x.productName, { lowecase: true }))
+console.log(slugs);
+console.log(slugify('Fresh Avocados', { lowecase: true}));
 
 const server=http.createServer((req, res)=>{
     const {query, pathname}= url.parse(req.url, true)
