b396f2aa862afa9eb3ec6d6bb71fc9348bc73707
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-05-31 13:32:24 -0500

    basic crud

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,18 +1,115 @@
+const fs = require('fs')
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res)=>{
+app.use(express.json())
+// app.get('/', (req, res)=>{
+//     res
+//     .status(200)
+//     .json({
+//         message: 'Hello from the server side!',
+//         app: 'Natours'
+//     })
+// })
+
+// app.post('/', (req, res)=>{
+//     res.send('You can post to this endpoint')
+// })
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+
+app.get('/api/v1/tours', (req, res)=>{
     res
     .status(200)
     .json({
-        message: 'Hello from the server side!',
-        app: 'Natours'
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
     })
 })
 
-app.post('/', (req, res)=>{
-    res.send('You can post to this endpoint')
+app.get('/api/v1/tours/:id', (req, res)=>{
+    const id = req.params.id * 1
+    const tour  = tours.find( el => el.id === id)
+    if(!tour){
+        return res
+        .status(404)
+        .json({
+            status: 'fail',
+            message: 'Invalid Id'
+        })
+    }
+    console.log(tour)
+    res
+    .status(200)
+    .json({
+        status: 'success',
+        data:{
+            tour
+        }
+    })
+})
+
+app.post('/api/v1/tours', (req, res)=>{
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({ id: newId }, req.body)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err=>{
+        res
+        .status(201)
+        .json({
+            status: 'success',
+            data:{
+                tour: newTour
+            }
+        })
+    })
+})
+
+app.patch('/api/v1/tours/:id', (req, res)=>{
+    const id = req.params.id * 1
+    const tour  = tours.find( el => el.id === id)
+    if(!tour){
+        return res
+        .status(404)
+        .json({
+            status: 'fail',
+            message: 'Invalid Id'
+        })
+    }
+    res
+    .status(200)
+    .json({
+        status: 'success',
+        data: {
+            tour: 'Updated tour'
+        }
+    })
+})
+
+app.delete('/api/v1/tours/:id', (req, res)=>{
+    const id = req.params.id * 1
+    const tour  = tours.find( el => el.id === id)
+    if(!tour){
+        return res
+        .status(404)
+        .json({
+            status: 'fail',
+            message: 'Invalid Id'
+        })
+    } 
+    res
+    .status(204)
+    .json({
+        status: 'success',
+        data: null
+    })
 })
 
 const port = 3000
