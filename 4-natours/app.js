9729e43505893fcf62c12cb35b9bcd93ea11de38
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-06-02 01:20:56 -0500

    middlewares

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,28 +1,31 @@
 const fs = require('fs')
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
+// 1) MIDDLEWARES
+app.use(morgan('dev'))
 app.use(express.json())
-// app.get('/', (req, res)=>{
-//     res
-//     .status(200)
-//     .json({
-//         message: 'Hello from the server side!',
-//         app: 'Natours'
-//     })
-// })
+app.use((req, res, next)=>{
+    console.log('Hello from the middleware!')
+    next()
+})
 
-// app.post('/', (req, res)=>{
-//     res.send('You can post to this endpoint')
-// })
+app.use((req, res, next)=>{
+    req.requestTime = new Date().toISOString()
+    next()
+})
+
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 )
 
 
-app.get('/api/v1/tours', (req, res)=>{
+// 2) ROUTES HANDLERS
+const getAllTours = (req, res)=>{
+    console.log(req.requestTime)
     res
     .status(200)
     .json({
@@ -32,9 +35,9 @@ app.get('/api/v1/tours', (req, res)=>{
             tours
         }
     })
-})
+}
 
-app.get('/api/v1/tours/:id', (req, res)=>{
+const getTour = (req, res) =>{
     const id = req.params.id * 1
     const tour  = tours.find( el => el.id === id)
     if(!tour){
@@ -45,7 +48,6 @@ app.get('/api/v1/tours/:id', (req, res)=>{
             message: 'Invalid Id'
         })
     }
-    console.log(tour)
     res
     .status(200)
     .json({
@@ -54,9 +56,9 @@ app.get('/api/v1/tours/:id', (req, res)=>{
             tour
         }
     })
-})
+}
 
-app.post('/api/v1/tours', (req, res)=>{
+const createTour = (req, res)=>{
     const newId = tours[tours.length - 1].id + 1
     const newTour = Object.assign({ id: newId }, req.body)
     tours.push(newTour)
@@ -70,9 +72,9 @@ app.post('/api/v1/tours', (req, res)=>{
             }
         })
     })
-})
+}
 
-app.patch('/api/v1/tours/:id', (req, res)=>{
+const updateTour = (req, res)=>{
     const id = req.params.id * 1
     const tour  = tours.find( el => el.id === id)
     if(!tour){
@@ -91,9 +93,9 @@ app.patch('/api/v1/tours/:id', (req, res)=>{
             tour: 'Updated tour'
         }
     })
-})
+}
 
-app.delete('/api/v1/tours/:id', (req, res)=>{
+const deleteTour = (req, res) =>{
     const id = req.params.id * 1
     const tour  = tours.find( el => el.id === id)
     if(!tour){
@@ -110,7 +112,30 @@ app.delete('/api/v1/tours/:id', (req, res)=>{
         status: 'success',
         data: null
     })
-})
+}
+
+
+//3) ROUTES
+
+//app.get('/api/v1/tours', getAllTours)
+// app.get('/api/v1/tours/:id', getTour)
+// //app.post('/api/v1/tours', createTour)
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id', deleteTour)
+
+app
+    .route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createTour)
+
+app
+    .route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour)
+
+
+//4) START SERVER
 
 const port = 3000
 app.listen(port, ()=>{
