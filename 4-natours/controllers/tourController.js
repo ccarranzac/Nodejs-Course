3fb8b2a04b13f0df3298a371c7fa4d26ecdf14c5
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-06-07 01:30:42 -0500

    eslint config

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,89 +1,83 @@
-const fs = require('fs')
+const fs = require('fs');
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-)
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
 
-exports.checkID = (req, res, next, value)=>{
-    if(value * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid Id'
-        })
-    }
-    next()
-}
+exports.checkID = (req, res, next, value) => {
+  if (value * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  next();
+};
 
-exports.checkBody = (req, res, next)=>{
-    const requestBody = req.body
-    if(!requestBody.name || !requestBody.price){
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Missing name or price'
-        })
-    }
-    next()
-}
+exports.checkBody = (req, res, next) => {
+  const requestBody = req.body;
+  if (!requestBody.name || !requestBody.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
 
-exports.getAllTours = (req, res)=>{
-    console.log(req.requestTime)
-    res
-    .status(200)
-    .json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
-}
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
 
-exports.getTour = (req, res) =>{
-    const id = req.params.id * 1
-    const tour  = tours.find( el => el.id === id)
-    res
-    .status(200)
-    .json({
-        status: 'success',
-        data:{
-            tour
-        }
-    })
-}
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
 
-exports.createTour = (req, res)=>{
-    const newId = tours[tours.length - 1].id + 1
-    const newTour = Object.assign({ id: newId }, req.body)
-    tours.push(newTour)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err=>{
-        res
-        .status(201)
-        .json({
-            status: 'success',
-            data:{
-                tour: newTour
-            }
-        })
-    })
-}
-
-exports.updateTour = (req, res)=>{  
-    res
-    .status(200)
-    .json({
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  // eslint-disable-next-line prefer-object-spread
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    () => {
+      res.status(201).json({
         status: 'success',
         data: {
-            tour: 'Updated tour'
-        }
-    })
-}
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
 
-exports.deleteTour = (req, res) =>{
-    res
-    .status(204)
-    .json({
-        status: 'success',
-        data: null
-    })
-}
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated tour',
+    },
+  });
+};
 
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
