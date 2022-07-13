df8085de469d7504411a65bb224d57280a5250e7
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-07-12 21:28:23 -0500

    arrays and objects

--- play.js
+++ play.js
@@ -27,7 +27,26 @@ function sumarizeUser(userName, userAge, userHasHobby) {
 const add = (a, b) => a + b;
 const addRandom = () => 1 + 2;
 
-console.log(sumarizeUser(name, age, hasHobbies));
-console.log(sumUser(name, age, hasHobbies));
-console.log(add(1, 2));
-console.log(addRandom());
+// console.log(sumarizeUser(name, age, hasHobbies));
+// console.log(sumUser(name, age, hasHobbies));
+// console.log(add(1, 2));
+// console.log(addRandom());
+
+const person = {
+  name: "Max",
+  age: 22,
+  greet() {
+    console.log("hi, i am " + this.name);
+  },
+};
+
+console.log(person);
+person.greet();
+
+let hobbies = ["Sports", "Cooking"];
+for (let item of hobbies) {
+  console.log(item);
+}
+
+hobbies = hobbies.map((el) => "Hobby " + el);
+console.log(hobbies);
