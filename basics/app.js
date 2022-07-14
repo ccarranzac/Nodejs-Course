2fdc0a787722dc5fecbbfbc6796095aa3d757bcc
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-07-14 12:25:21 -0500

    blocking and non blocking code

--- basics/app.js
+++ basics/app.js
@@ -1,11 +1,40 @@
 const http = require("http");
+const fs = require("fs");
 
 //execute this function with every incoming request
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></from></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFile("text.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
   res.setHeader("Content-type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from Nodejs server</h1></body>");
+  res.write("<body><h1>Hello from nodejs server!!</h1></body>");
   res.write("</html>");
   res.end();
 });
