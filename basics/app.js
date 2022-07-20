966c82779541ad77444ba17ed77bb0548f4d821e
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-07-19 20:05:27 -0500

    adding nodemon

--- basics/app.js
+++ basics/app.js
@@ -2,7 +2,6 @@ const http = require("http");
 
 const routes = require("./routes");
 
-console.log(routes.someText);
 //execute this function with every incoming request
 const server = http.createServer(routes.handler);
 
