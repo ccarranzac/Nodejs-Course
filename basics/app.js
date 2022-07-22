b2cf0c2f330244569a4cc7cd0a6fea49a57715a3
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-07-22 14:02:13 -0500

    express and middlewares

--- basics/app.js
+++ basics/app.js
@@ -1,8 +1,21 @@
-const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
+const app = express();
+
+app.use("/", (req, res, next) => {
+  console.log("always runs");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In other midleware!");
+  res.send("<h1>The 'Add product page'</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In other midleware!");
+  res.send("<h1>Hello from express</h1>");
+});
 
 //execute this function with every incoming request
-const server = http.createServer(routes.handler);
-
-server.listen(3000);
+app.listen(3000);
