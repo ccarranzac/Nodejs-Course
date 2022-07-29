f7de186cb87d6c37436e7a243b63b4dda1b34474
Camilo Andrés Carranza <ccarranzac@unal.edu.co>
2022-07-28 20:32:57 -0500

    ejs

--- basics/app.js
+++ basics/app.js
@@ -1,18 +1,18 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.engine(
-  "handlebars",
-  expressHbs({ layoutsDir: "views/layouts/", defaultLayout: "main-layout" })
-);
-app.set("view engine", "handlebars");
+// app.engine(
+//   "handlebars",
+//   expressHbs({ layoutsDir: "views/layouts/", defaultLayout: "main-layout" })
+// );
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +23,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.render("404", { pageTitle: "Page Not Found" });
+  res.render("404", { pageTitle: "Page Not Found", path: "" });
   //res.sendFile(path.join(__dirname, "views", "404.html"));
 });
 
